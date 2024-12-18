f79ab713207c21394cee2eac81e63e9397225ae0
Andrew Peterson <andrewpetersondev@gmail.com>
2024-12-17 21:09:32 -0600

    Refactor Navbar component and add ProfileDropdown; introduce navigation utility

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 
-import Navbar from "../components/Navbar";
+import Navbar from "../components/Navbar/Navbar";
+import Navbar2 from "../components/Navbar/Navbar2";
 
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -31,6 +32,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Navbar />
+        <Navbar2 />
         {children}
       </body>
     </html>
